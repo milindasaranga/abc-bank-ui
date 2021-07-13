3309b346b9466327eedc40e1b52aea169ef43db2
“milindadev” <msck.dev@gmail>
2021-07-13 10:12:49 +0400

    register option

--- src/app/shared/service.urls.ts
+++ src/app/shared/service.urls.ts
@@ -1,6 +1,6 @@
 export class ServiceUrls{
-    public static HOST = "http://localhost:8080/";
-    //public static HOST = "https://abc-bank-milinda.herokuapp.com/";
+    //public static HOST = "http://localhost:8080/";
+    public static HOST = "https://abc-bank-milinda.herokuapp.com/";
     
     public static AUTH_API = "login/authenticate";
     public static USERS_API = "user";
